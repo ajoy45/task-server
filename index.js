62d511b1cde244f902c68baa89d3a2bbae880bed
ajoy45 <ajoypust@gmail.com>
2023-08-04 16:37:24 +0600

    delete method complete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 // pass:FMxAwIGN2FAoo7BI
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmip1b8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,10 +40,34 @@ async function run() {
         })
 
         // get all task from database
-        app.get('/all-task',async(req,res)=>{
-            const result=await taskCollection.find().toArray();
+        app.get('/all-task', async (req, res) => {
+            const result = await taskCollection.find().toArray();
             res.send(result)
         })
+        // update status
+        app.put('/add-task/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateTask = req.body;
+            console.log(updateTask)
+            const updateDoc = {
+                $set: {
+                    status: updateTask.status,
+                   
+                }
+            }
+            const result = await taskCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+        // delete task
+        app.delete('/add-task/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:new ObjectId(id)};
+            const result=await taskCollection.deleteOne(query);
+            res.send(result)
+          })
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
